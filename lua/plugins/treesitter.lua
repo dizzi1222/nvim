fa574883b2ef9118b56805e18c5548cd89e1ce3c
Diego Härdi <127579140+dizzi1222@users.noreply.github.com>
2026-01-15 10:23:56 -0400

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,41 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    highlight = {
-      enable = true,
-      disable = { "snacks_picker_list", "snacks_picker", "snacks_input", "snacks_notif" },
+-- Detectar plataforma
+local is_wsl = vim.fn.has("wsl") == 1
+local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+local is_linux = vim.fn.has("unix") == 1 and not is_wsl
+
+if is_wsl or is_windows then
+  -- PARA WINDOWS O WSL
+  return {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        -- Silenciar errores de treesitter
+        -- No recomendable, pero no me quedo de otra [En Windows].
+        local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+        if not status_ok then
+          return
+        end
+
+        treesitter.setup({
+          auto_install = true,
+          indent = { enable = true },
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+          },
+        })
+      end,
     },
-  },
-}
+  }
+else
+  -- PARA LINUX
+  return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = {
+        enable = true,
+        disable = { "snacks_picker_list", "snacks_picker", "snacks_input", "snacks_notif" },
+      },
+    },
+  }
+end
